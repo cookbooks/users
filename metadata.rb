e099fe47248aac460029972a3fe9fd43db527dc4
Craig S. Cottingham <craig.cottingham@gmail.com>
2011-04-08 11:55:40 -0500

    Add default recipe to metadata
    Signed-off-by: Craig S. Cottingham <craig.cottingham@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,5 @@ license          "Apache 2.0"
 description      "Creates users from a databag search"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.3.1"
+recipe "users", "reads users data bag and creates users",
 recipe "users::sysadmin", "searches users data bag for sysadmins and creates users"
